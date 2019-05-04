0894addb4de928f578763d94665444d40821eb6f
Daniël Heres <dheres@bol.com>
2019-05-04 16:51:51 +0200

    Add simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+fn index() -> impl Responder {
+    format!("Hello")
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(web::resource("/").to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
 }
